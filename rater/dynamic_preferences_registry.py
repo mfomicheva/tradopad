b0b0868ef189971808af0ff614ff8b9298434cb7
Anton Malinovskiy <anton@nuroa.com>
2015-12-15 06:07:14 +0100

    added help text

--- rater/dynamic_preferences_registry.py
+++ rater/dynamic_preferences_registry.py
@@ -56,6 +56,16 @@ class ContinuousScaleMinLabel(StringPreference):
 
 
 @global_preferences_registry.register
+class HelpText(StringPreference):
+    section = preferences_section
+    name = 'help_text'
+    default = '''In this evaluation task you will assess the quality of Chinese-English machine translations of one hundred sentences. Instead of the original Chinese sentences, professional human translations will be used as benchmark for evaluation. Specifically, you will be asked how much of the meaning of the human translation is expressed in the machine translation. <br /><br />
+Your email will only be used as an identifier for this particular task and will be discarded as soon as we collect the data.<br /><br />
+If you have to leave off evaluating and get back to the task later, you can log in again using your email and you will return to the sentence where you left off.'''
+    help_text = 'introductory text to be displayed on hte first page'
+
+
+@global_preferences_registry.register
 class ContinuousScaleMaxLabel(StringPreference):
     section = preferences_section
     name = 'continuous_scale_max_label'
